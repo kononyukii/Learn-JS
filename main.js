2d52171775683a1085dc82ff4becb0089eef0911
kononyukii <kononyukii@gmail.com>
2020-10-16 17:02:05 +0300

    Limiting greed in regexes in JavaScript

--- main.js
+++ main.js
@@ -1,26 +1,11 @@
 "use strict";
 
 //Дана строка:
-let str = 'aa aba abba abbba abbbba abbbbba';
-/*Напишите регулярку, которая найдет строки 
-'abba', 'abbba', 'abbbba' и только их.*/
+let str = 'aba accca azzza wwwwa';
+/*Напишите регулярку, которая найдет все строки по краям которых 
+стоят буквы 'a', и заменит каждую из них на '!'. 
+Между буквами a может быть любой символ (кроме 'a').*/
 
-let res = str.replace(/ab{2,4}a/g, '!');
-
-
-//Дана строка:
-str = 'aa aba abba abbba abbbba abbbbba';
-/*Напишите регулярку, которая найдет строки вида 'aba', в которых 'b' 
-встречается менее 3-х раз (включительно).*/
-
-res = str.replace(/ab{0,3}a/g, '!');
-
-
-//Дана строка:
-str = 'aa aba abba abbba abbbba abbbbba';
-/*Напишите регулярку, которая найдет строки вида 'aba', 
-в которых 'b' встречается более 4-х раз (включительно).*/
-
-res = str.replace(/ab{4,}a/g, '!');
+let res = str.replace(/a.+?a/g, '!');
 
 console.log(res);
