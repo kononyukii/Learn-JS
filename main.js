74214a7b9cb09132e70285bd7c96b0f96c7866bf
kononyukii <kononyukii@gmail.com>
2020-10-22 16:30:41 +0300

    Pockets in the regex itself in JavaScript

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
 "use strict";
 
 //Дана строка:
-let str = 'a1b2c3';
-//Напишите регулярку, которая рядом с каждой цифрой напишет такую же.
-let res = str.replace(/(\d)/g, '$&$&');
+let str = 'aaa bbb ccc xyz';
+//Найдите все подстроки, в которых есть три одинаковые буквы подряд.
+let res = str.replace(/([a-z])\1\1/g, '!');
 
 console.log(res);
 
 
 //Дана строка:
-str = 'sss site.ru zzz site.com kkk';
-//Замените домены на ссылки вида '<a href="http://site.ru">site.ru</a>'.
-res = str.replace(/[a-z]+\.[a-z]{2,3}/g, '<a href="http://$&">$&</a>');
+str = 'a aa aaa aaaa aaaaa';
+/* Найдите все подстроки, 
+в которых есть две и более одинаковые буквы подряд. */
+
+res = str.replace(/([a-z])\1+/g, '!');
+
+console.log(res);
+
+
+//Дана строка:
+str = 'aaa aaa bbb bbb ccc ddd';
+//Найдите все подстроки, в которых есть два одинаковых слова подряд.
+
+res = str.replace(/([a-z]+) \1/g, '!');
 
 console.log(res);
