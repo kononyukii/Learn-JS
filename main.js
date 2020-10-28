fc857f1b676dc0e2cdd817e9652e49c36a62588d
kononyukii <kononyukii@gmail.com>
2020-10-28 15:20:20 +0200

    dragover

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-document.querySelector('#elem').addEventListener('drag', function (event) {
+/* document.querySelector('#elem').addEventListener('drag', function (event) {
 	console.log('!')
+}); */
+
+let parent = document.querySelector('#parent');
+
+parent.addEventListener('dragover', function (event) {
+	this.style.borderStyle = 'dotted';
 });
